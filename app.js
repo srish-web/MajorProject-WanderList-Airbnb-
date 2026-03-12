cadf57cbbac277c7ff4d647944cff13d6268db31
srish-web <mssrishtygupta1110@gmail.com>
2026-03-12 20:22:32 +0530

    styling

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ const mongoose = require("mongoose");
 const listing = require("./models/listing.js")
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname, "public")));
 
 async function main(){
     await mongoose.connect('mongodb://127.0.0.1:27017/WanderList');
